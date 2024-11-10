bbd352da18328b69a45eb500b61cffe7c50ddf16
monte-v <vdmitry08@gmail.com>
2024-11-10 16:59:44 +0300

    Создание абстрактного класса Shape и добавление в него метода GetArea для получения площади

---
+++ opp_4_Abstraction/Shape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opp_4_Abstraction
+{
+    abstract class Shape
+    {
+        // Абстрактный метод для получения площади
+        public abstract double GetArea();
+    }
+}
