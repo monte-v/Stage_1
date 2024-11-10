9f94f4056f788d586d3a06cce238c1eb8a04e2be
monte-v <vdmitry08@gmail.com>
2024-11-10 18:22:44 +0300

    Создание класса AllInOnePrinter с методами Print и Scan, унаследованного от интерфейса IMultifunctionDevice

---
+++ solid_4_ISP/AllInOnePrinter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid_4_ISP
+{
+    internal class AllInOnePrinter : IMultifunctionDevice
+    {
+        public void Print(string document)
+        {
+            Console.WriteLine($"Печать документа: {document} с помощью многофункционального устройства.");
+        }
+
+        public void Scan(string document)
+        {
+            Console.WriteLine($"Сканирование документа: {document} с помощью многофункционального устройства.");
+        }
+    }
+}
