9e1184cc726c2927646684777e222dd749635904
monte-v <vdmitry08@gmail.com>
2024-11-10 16:41:42 +0300

    Создание класса Animal и добавление в него метода MakeSound, который будет переопределен в подклассах

---
+++ opp_3_Polymorphism/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opp_3_Polymorphism
+{
+    internal class Animal
+    {
+        // Метод, который будет переопределен в подклассах
+        public virtual string MakeSound()
+        {
+            return "Некоторый звук";
+        }
+    }
+}
