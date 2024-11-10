023e5ddc8580be32904d36fe64e14809851064a4
monte-v <vdmitry08@gmail.com>
2024-11-10 17:29:38 +0300

    Создание абстрактного класса Discount и добавление в него метода Calculate для расчета скидки

---
+++ solid_2_OCP/Discount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid_2_OCP
+{
+    abstract class Discount
+    {
+        // Абстрактный метод для расчета скидки
+        public abstract double Calculate(double originalPrice);
+    }
+}
