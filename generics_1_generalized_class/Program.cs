283ed6834d87f2cab5a38e3a0ff57fa79ad0ba7b
monte-v <vdmitry08@gmail.com>
2024-11-10 19:47:17 +0300

    Использование класса Pair

--- generics_1_generalized_class/Program.cs
+++ generics_1_generalized_class/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using generics_1_generalized_class;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pair<int, string> pair1 = new Pair<int, string>(1, "Первый");
+        Console.WriteLine(pair1); 
+
+        // Изменение значений
+        pair1.First = 2;
+        pair1.Second = "Второй";
+        Console.WriteLine(pair1); 
+
+        Pair<string, double> pair2 = new Pair<string, double>("Третий", 3.14);
+        Console.WriteLine(pair2); 
+    }
+}
