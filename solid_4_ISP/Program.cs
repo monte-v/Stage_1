a2e7ebbe4081e14a1d14240e26a8f0f94f7371d4
monte-v <vdmitry08@gmail.com>
2024-11-10 18:23:39 +0300

    Печать документов

--- solid_4_ISP/Program.cs
+++ solid_4_ISP/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using solid_4_ISP;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IPrinter laserPrinter = new LaserPrinter();
+        laserPrinter.Print("Документ для печати");
+
+        IMultifunctionDevice allInOnePrinter = new AllInOnePrinter();
+        allInOnePrinter.Print("Документ для печати с многофункционального устройства");
+        allInOnePrinter.Scan("Документ для сканирования");
+    }
+}
