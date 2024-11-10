ea9d9a4f647a14f76bcea69c18b902a63b012f8e
monte-v <vdmitry08@gmail.com>
2024-11-10 20:32:05 +0300

    Создание обобщенного метода Swap для обмена значениями

---
+++ generics_4_generalized_method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+    // Обобщенный метод для обмена значениями
+    public static void Swap<T>(ref T first, ref T second)
+    {
+        T temp = first;  
+        first = second; 
+        second = temp;   
+    }
+
+    public static void Main(string[] args)
+    {
+        int a = 5;
+        int b = 10;
+        Console.WriteLine($"До обмена: a = {a}, b = {b}");
+        Swap(ref a, ref b);
+        Console.WriteLine($"После обмена: a = {a}, b = {b}");
+
+    }
+}
