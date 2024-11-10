77c021ffb5f462ee769c4254a6317d07099c3d88
monte-v <vdmitry08@gmail.com>
2024-11-10 20:23:34 +0300

    Создание пример класса Person

--- generics_3_generalized_class-collection/Program.cs
+++ generics_3_generalized_class-collection/Program.cs
@@ -6,7 +6,18 @@ class Program
 {
     static void Main(string[] args)
     {
+        Repository<Person> personRepo = new Repository<Person>();
 
+        personRepo.Add(new Person("Alice"));
+        personRepo.Add(new Person("Bob"));
+
+        Console.WriteLine(personRepo.Get(0).Name); 
+        Console.WriteLine(personRepo.Get(1).Name); 
+
+        foreach (var person in personRepo.GetAll())
+        {
+            Console.WriteLine(person.Name);
+        }
     }
 }
 
