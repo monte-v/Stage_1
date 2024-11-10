87c96b791f058a0eef242ce8d6ecce9dccac6d36
monte-v <vdmitry08@gmail.com>
2024-11-10 19:46:30 +0300

    Создание класса Pair со свойствами получения и установки значениий и методом ToString для вывода пары в строковом формате

---
+++ generics_1_generalized_class/Pair.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics_1_generalized_class
+{
+    internal class Pair<T, U>
+    {
+        private T _first;
+        private U _second;
+
+        public Pair(T first, U second)
+        {
+            _first = first;
+            _second = second;
+        }
+
+        // Свойство для получения и установки первого значения
+        public T First
+        {
+            get { return _first; }
+            set { _first = value; }
+        }
+
+        // Свойство для получения и установки второго значения
+        public U Second
+        {
+            get { return _second; }
+            set { _second = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Pair: ({First}, {Second})";
+        }
+    }
+}
