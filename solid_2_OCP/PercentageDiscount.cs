3d39955d2ba9e7fe0b1e237e4253eb7a285e7e56
monte-v <vdmitry08@gmail.com>
2024-11-10 17:33:52 +0300

    Создание, унаследованного от абстрактного Discount, класса PercentageDiscount и добавление в него метода Calculate для получения процентной скидки

---
+++ solid_2_OCP/PercentageDiscount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid_2_OCP
+{
+    internal class PercentageDiscount : Discount
+    {
+        public double Percentage { get; set; } // Процент скидки
+
+        public PercentageDiscount(double percentage)
+        {
+            Percentage = percentage;
+        }
+
+        // Реализация метода Calculate для процентной скидки
+        public override double Calculate(double originalPrice)
+        {
+            return originalPrice - (originalPrice * (Percentage / 100));
+        }
+    }
+}
--- solid_2_OCP/Program.cs
+++ solid_2_OCP/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using solid_2_OCP;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double originalPrice = 1000; // Оригинальная цена
+
+        // Создаем объекты скидок
+        Discount percentageDiscount = new PercentageDiscount(10); // 10% скидка
+
+        // Высчитываем и выводим цену со скидкой
+        double priceAfterPercentageDiscount = percentageDiscount.Calculate(originalPrice);
+
+        Console.WriteLine($"Цена после процентной скидки: {priceAfterPercentageDiscount}");
+    }
+}
