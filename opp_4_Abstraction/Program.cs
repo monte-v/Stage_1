632ea737fa0cf66735ff3bce0a25ea198737a0cb
monte-v <vdmitry08@gmail.com>
2024-11-10 17:06:04 +0300

    Создание, унаследованного от абстрактного Shape, класса Rectangle и добавление в него метода GetArea для получения площади прямоугольника

--- opp_4_Abstraction/Program.cs
+++ opp_4_Abstraction/Program.cs
@@ -1,7 +1,18 @@
-﻿class Program
+﻿using opp_4_Abstraction;
+
+class Program
 {
     static void Main(string[] args)
-    { 
-    
+    {
+        Shape[] shapes = new Shape[]
+            {
+            new Circle(5),       // Круг с радиусом 5
+            new Rectangle(4, 6)  // Прямоугольник с шириной 4 и высотой 6
+            };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Площадь: {shape.GetArea()}");
+        }
     }
 }
