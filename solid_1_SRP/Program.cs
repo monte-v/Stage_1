bd786e61e8e7aac27735de355c62e1ffe1e4ffc5
monte-v <vdmitry08@gmail.com>
2024-11-10 17:14:45 +0300

    Создание класса Employee и добавление в него метода GetInfo для получения информации о сотруднике

---
+++ solid_1_SRP/Program.cs
@@ -0,0 +1,11 @@
+﻿using solid_1_SRP;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee employee = new Employee("Иван Иванов", "Разработчик", 50000);
+
+        Console.WriteLine(employee.GetInfo());
+    }
+}
