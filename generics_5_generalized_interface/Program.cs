6178bf858ac080c9297137fcce121dabfd754e06
monte-v <vdmitry08@gmail.com>
2024-11-10 20:40:39 +0300

    Пример использования класса ListStorage

--- generics_5_generalized_interface/Program.cs
+++ generics_5_generalized_interface/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using generics_5_generalized_interface;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IStorage<int> intStorage = new ListStorage<int>();
+        intStorage.Add(10);
+        intStorage.Add(20);
+        intStorage.Add(30);
+
+        Console.WriteLine($"Элемент с индексом 1: {intStorage.Get(1)}"); // Вывод: 20
+    }
+}
