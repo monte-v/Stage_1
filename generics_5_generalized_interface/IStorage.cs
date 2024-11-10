7c54f34ffac8e61464527d1c8c48591c8a7a1f03
monte-v <vdmitry08@gmail.com>
2024-11-10 20:38:06 +0300

    Создание интерфейса с методами Add для добавления элемента и Get для получения элемента по индексу

---
+++ generics_5_generalized_interface/IStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics_5_generalized_interface
+{
+    internal interface IStorage<T>
+    {
+        void Add(T item);
+        T Get(int index);
+    }
+}
