cb2ec6b3b7c07d2f382360939574a7802a29bb00
monte-v <vdmitry08@gmail.com>
2024-11-10 20:39:46 +0300

    Создание класса ListStorage и реализация методов Add и Get

---
+++ generics_5_generalized_interface/ListStorage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics_5_generalized_interface
+{
+    public class ListStorage<T> : IStorage<T>
+    {
+        private List<T> _items;
+
+        public ListStorage()
+        {
+            _items = new List<T>(); 
+        }
+
+        // Реализация метода Add
+        public void Add(T item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item), "Элемент не может быть null");
+            }
+            _items.Add(item); 
+        }
+
+        // Реализация метода Get
+        public T Get(int index)
+        {
+            if (index < 0 || index >= _items.Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "Индекс находится за пределами диапазона");
+            }
+            return _items[index]; 
+        }
+    }
+}
