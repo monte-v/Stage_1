1f4c3ab34e65889f63b49e2fa0cba3a30ecce7b0
monte-v <vdmitry08@gmail.com>
2024-11-10 17:19:00 +0300

    Создание класса SalaryCalculator и добавление в него методов CalculateSalary для расчета зарплаты с учетом бонусов и CalculateAnnualSalary для расчета годовой зарплаты

--- solid_1_SRP/Program.cs
+++ solid_1_SRP/Program.cs
@@ -7,5 +7,14 @@ class Program
         Employee employee = new Employee("Иван Иванов", "Разработчик", 50000);
 
         Console.WriteLine(employee.GetInfo());
+
+        SalaryCalculator salaryCalculator = new SalaryCalculator();
+
+        double bonus = 10000;
+        double salary = salaryCalculator.CalculateSalary(employee, bonus);
+        Console.WriteLine($"Зарплата с учетом бонуса: {salary}");
+
+        double annualSalary = salaryCalculator.CalculateAnnualSalary(employee, bonus);
+        Console.WriteLine($"Годовая зарплата с учетом бонуса: {annualSalary}");
     }
 }
