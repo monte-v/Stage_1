fb752f5a382d586031a1e85615aa173afcd00df3
monte-v <vdmitry08@gmail.com>
2024-11-10 15:39:28 +0300

    Создание класса BankAccount

---
+++ opp_1_BankAcoount/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opp_1_BankAcoount
+{
+    internal class BankAccount
+    {
+        // Поле для хранения баланса (инкапсуляция)
+        private decimal balance;
+
+        // Конструктор для инициализации счета с начальным балансом
+        public BankAccount(decimal initialBalance)
+        {
+            if (initialBalance < 0)
+            {
+                throw new ArgumentException("Начальный баланс не может быть отрицательным.");
+            }
+            balance = initialBalance;
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ opp_1_BankAcoount/Program.cs
@@ -0,0 +1,11 @@
+﻿using opp_1_BankAcoount;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount(1000); 
+
+        Console.WriteLine($"Текущий баланс: {account.GetBalance()}");
+    }
+}
