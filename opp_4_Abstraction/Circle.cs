592c00cfff5839ba08a2ac9674b7368ce04872ca
monte-v <vdmitry08@gmail.com>
2024-11-10 17:02:57 +0300

    Создание, унаследованного от абстрактного Shape, класса Circle и добавление в него метода GetArea для получения площади круга

---
+++ opp_4_Abstraction/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opp_4_Abstraction
+{
+    internal class Circle : Shape
+    {
+        public double Radius { get; set; } 
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius; 
+        }
+    }
+}
