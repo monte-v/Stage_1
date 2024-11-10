57dd4ecd09a681bab1112a32cdc7eac110504452
monte-v <vdmitry08@gmail.com>
2024-11-10 16:27:16 +0300

    Создание, унаследованного от Vehicle, класса Car и добавление в него метода GetInfo для получения информации об автомобиле

---
+++ opp_2_Inheritance/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opp_2_Inheritance
+{
+    internal class Car : Vehicle
+    {
+        // Уникальные свойства для автомобиля
+        public int NumberOfSeats { get; set; } // Количество мест
+        public string TransmissionType { get; set; } // Тип трансмиссии 
+
+        // Конструктор
+        public Car(string make, string model, int year, int numberOfSeats, string transmissionType)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            NumberOfSeats = numberOfSeats;
+            TransmissionType = transmissionType;
+        }
+
+        // Метод для получения информации
+        public override string GetInfo()
+        {
+            return base.GetInfo() + $", Количество мест: {NumberOfSeats}, Тип трансмиссии: {TransmissionType}";
+        }
+    }
+}
--- opp_2_Inheritance/Program.cs
+++ opp_2_Inheritance/Program.cs
@@ -1,6 +1,11 @@
-﻿class Program
+﻿using opp_2_Inheritance;
+
+class Program
 {
     static void Main(string[] args)
     {
+        Car myCar = new Car("Toyota", "Camry", 2022, 5, "Автоматическая");
+
+        Console.WriteLine(myCar.GetInfo());
     }
 }
