2f7a36396e68a4d7292e29c41fb358165a83e6c8
monte-v <vdmitry08@gmail.com>
2024-11-10 16:20:43 +0300

    Создание класса Vehicle и добавление в него метода GetInfo для получения информации о транспортном средстве

---
+++ opp_2_Inheritance/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opp_2_Inheritance
+{
+    internal class Vehicle
+    {
+        public string Make { get; set; }  // Производитель
+        public string Model { get; set; } // Модель
+        public int Year { get; set; }      // Год выпуска
+
+        // Метод для получения информации о транспортном средстве
+        public virtual string GetInfo()
+        {
+            return $"Производитель: {Make}, Модель: {Model}, Год: {Year}";
+        }
+    }
+}
