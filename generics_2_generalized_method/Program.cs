9f79b571a03e7f43e541bd2c793bb26063e48173
monte-v <vdmitry08@gmail.com>
2024-11-10 19:58:10 +0300

    Создание обобщенного метода FindMin для поиска минимального значения

---
+++ generics_2_generalized_method/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+    public static T FindMin<T>(T[] array) where T : IComparable<T>
+    {
+        if (array == null || array.Length == 0)
+        {
+            throw new ArgumentException("Массив не должен быть пустым или равным null");
+        }
+
+        T min = array[0]; 
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            // Сравниваем current элемент с текущим минимальным
+            if (array[i].CompareTo(min) < 0)
+            {
+                min = array[i]; 
+            }
+        }
+
+        return min; 
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] intArray = { 5, 3, 8, 1, 4 };
+        Console.WriteLine($"Минимальное значение (int): {FindMin(intArray)}"); 
+    }
+}
